0b04c7c24746236c3a9d1b8fdb1557b82fe3d055
seanjoerick <mcdetails011@gmail.com>
2024-10-15 17:09:57 +0800

    add middleware proectedRoute

--- server/middleware/protectedRoute.js
+++ server/middleware/protectedRoute.js
@@ -1,40 +1,46 @@
-// import jwt from "jsonwebtoken";
-// import User from "../models/user.model.js";
+import jwt from 'jsonwebtoken';
+import prisma from '../prismaClient.js'; 
 
-// const protectRoute = async (req, res, next) => {
-//     try {
-//         // Retrieve JWT token from cookies
-//         const token = req.cookies.jwt;
+const protectRoute = async (req, res, next) => {
+    try {
+        // Retrieve JWT token from cookies
+        const token = req.cookies.jwt;
 
-//         // Check if token exists
-//         if (!token) {
-//             return res.status(401).json({ error: "Unauthorized - No Token Provided" });
-//         }
+        // Check if token exists
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized - No Token Provided' });
+        }
 
-//         // Verify token validity
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        // Verify token validity
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-//         // Check if token verification failed
-//         if (!decoded) {
-//             return res.status(401).json({ error: "Unauthorized - Invalid Token" });
-//         }
+        // Check if token verification failed
+        if (!decoded) {
+            return res.status(401).json({ error: 'Unauthorized - Invalid Token' });
+        }
 
-//         // Find user by decoded userId and exclude password field
-//         const user = await User.findById(decoded.userId).select("-password");
+        // Find user by decoded userId and exclude password field
+        const user = await prisma.user.findUnique({
+            where: {
+                user_id: decoded.userId,
+            },
+            select: {
+                password: false,
+            },
+        });
 
-//         // Check if user exists
-//         if (!user) {
-//             return res.status(404).json({ error: "User not found" });
-//         }
+        // Check if user exists
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
 
-//         req.user = user;
+        req.user = user;
 
-//         // Proceed to next middleware or route handler
-//         next();
-//     } catch (error) {
-//         console.log("Error in protectRoute middleware: ", error.message);
-//         res.status(500).json({ error: "Internal server error" });
-//     }
-// };
+        next();
+    } catch (error) {
+        console.log('Error in protectRoute middleware: ', error.message);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
 
-// export default protectRoute;
+export default protectRoute;
